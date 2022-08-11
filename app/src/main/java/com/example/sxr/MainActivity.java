ad90788ea3f9004200af603e44111202a2cb08b3
ming <1638196798@qq.com>
2022-08-12 00:18:23 +0800

    init:create new project

---
+++ app/src/main/java/com/example/sxr/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.sxr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+       final OkHttpClient client = new OkHttpClient();
+
+       TextView  textView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.myText);
+
+
+        getData();
+    }
+
+    private void getData() {
+        Request.Builder builder = new Request.Builder();
+        builder.url("https://open.douyin.com/discovery/ent/rank/item/");
+        final Request request = builder.build();
+
+        Call call = client.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    final String s = response.body().string();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(s);
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+}
